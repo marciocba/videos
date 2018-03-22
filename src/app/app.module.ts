3c2858def0a45b59dfa61cf2954b189dbbd99dd5
marciocba <marciocba@yahoo.com>
2018-03-22 09:37:09 -0300

    refactoring with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import {HttpModule} from '@angular/http';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { VideoCenterComponent } from './video-center/video-center.component';
-import { VideoListComponent } from './video-list/video-list.component';
-import { VideoDetailComponent } from './video-detail/video-detail.component';
-import { SafePipe } from './safe.pipe';
-import { LoginComponent } from './login/login.component';
+import { CoreModule } from './core/core.module';
+
+//import { HomeComponent } from './home/home.component';
+//import { VideoCenterComponent } from './video-center/video-center.component';
+//import { VideoListComponent } from './video-list/video-list.component';
+//import { VideoDetailComponent } from './video-detail/video-detail.component';
+//import { SafePipe } from './safe.pipe';
+import { HttpModule } from '@angular/http';
+//import { LoginComponent } from './login/login.component';
+//import { UserComponent } from './user/user.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    VideoCenterComponent,
-    VideoListComponent,
-    VideoDetailComponent,
-    SafePipe,
-    LoginComponent
+//    HomeComponent,
+//    VideoCenterComponent,
+//    VideoListComponent,
+//    VideoDetailComponent,
+//    SafePipe,
+//    LoginComponent,
+//    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule,HttpModule
+    CoreModule,
+    //FormsModule,HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
