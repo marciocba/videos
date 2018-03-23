a326add2ce9aaaa01b5ecbacd3a009ef32aff27c
marciocba <marciocba@yahoo.com>
2018-03-22 22:29:20 -0300

    removed components login sign and sigup

--- src/app/core/login/login.module.ts
+++ src/app/core/login/login.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
-import { LoginComponent } from './login.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { FormsModule } from '@angular/forms';
+//import { LoginComponent } from './login.component';
+//import { SignInComponent } from './sign-in/sign-in.component';
+//import { SignUpComponent } from './sign-up/sign-up.component';
+//import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule,FormsModule
+    LoginRoutingModule//,FormsModule
   ],
-  declarations: [LoginComponent,SignInComponent, SignUpComponent],
-  exports:[FormsModule]
+  declarations: [//SignInComponent, SignUpComponent
+    //,LoginComponent
+  ],
+  //exports:[FormsModule]
 })
 export class LoginModule { }
